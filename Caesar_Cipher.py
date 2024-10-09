66434f232ccf8fbf7333dda6330c7dd988106afe
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-10-09 10:34:41 +0000

    Please work this time

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,22 +1,27 @@
 def encrypt(word, shift):
     new_word = ""
     for letter in word:
-        letter = ord(letter)
-        letter += shift
-        if letter > 122:
-            letter -= 26
-        letter = chr(letter)
-        new_word += letter
+        if letter == " ":
+            new_word += " "
+        else:
+            letter = ord(letter)
+            letter += shift
+            if letter > 122:
+                letter -= 26
+            letter = chr(letter)
+            new_word += letter
     return new_word
 
 def unencrypt(word, shift):
     new_word = ""
     for letter in word:
-        letter = ord(letter)
-        letter -= shift
-        print(letter)
-        if letter < 97:
-            letter += 26
-        letter = chr(letter)
-        new_word += letter
+        if letter == " ":
+            new_word += " "
+        else:
+            letter = ord(letter)
+            letter -= shift
+            if letter < 97:
+                letter += 26
+            letter = chr(letter)
+            new_word += letter
     return new_word
